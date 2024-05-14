b327ac7898352d41af0b5d69ffc64016066bd2d0
Moseh-25-sudo <mosesnganga186@gmail.com>
2024-05-14 02:58:22 -0700

    Task03

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,17 +1,26 @@
 #!/usr/bin/python3
-"""
-Module 5-text_indentation
-Contain a method that print a text
-The text with 2 new lines after each of these characters: ., ? and :
-Takes one text argument
-"""
+"""Module for text_indentation method."""
+
 
 def text_indentation(text):
-    """a text with 2 new lines after each of these characters: ., ? and :"""
-    if type(text) is not str:
+    """Method for adding 2 new lines after '.?:' chars.
+
+    Args:
+        text: The str text.
+
+    Raises:
+        TypeError: If text is not a str.
+    """
+    if not isinstance(text, str):
         raise TypeError("text must be a string")
-    for char in ".?:":
-        text = text.replace(char, char + "\n\n")
-    new_line = [line.strip(' ') for line in text.split("\n")]
-    output = "\n".join(new_line)
-    print(output, end="")
+
+    for delim in ".?:":
+        # print(delim, text.split(delim))
+        text = (delim + "\n\n").join(
+            [line.strip(" ") for line in text.split(delim)])
+
+    print(text, end="")
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/5-text_indentation.txt")
