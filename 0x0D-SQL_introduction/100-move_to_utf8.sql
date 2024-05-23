20003c6c61f5960a9781174d1a141d7e0acd6d1f
Moseh-sudo-25 <mosesnganga186@gmail.com>
2024-05-23 04:10:35 -0700

    corr100

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,10 @@
+-- converts hbtn_0c_0 database to UTF8 utf8mb4, collate utf8mb4_unicode_ci in my MySQL server.
+
+SELECT name FROM tv_genres
+WHERE tv_genres.id NOT IN (
+      SELECT tv_genres.id FROM tv_genres
+      JOIN tv_show_genres ON tv_genres.id=tv_show_genres.genre_id
+      JOIN tv_shows ON tv_shows.id=tv_show_genres.show_id
+      WHERE tv_shows.title = "DEXTER" )
+ORDER BY tv_genres.name;
+
