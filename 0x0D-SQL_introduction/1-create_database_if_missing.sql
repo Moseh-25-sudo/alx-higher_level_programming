359b0aff8cc8cdfd59f32ae5698de5f08fd43e82
Moseh-sudo-25 <mosesnganga186@gmail.com>
2024-05-23 02:13:55 -0700

    corr

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,1 +1,6 @@
-CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
+-- The creates of the MySQL server for user user_0d_1.
+
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;
+
