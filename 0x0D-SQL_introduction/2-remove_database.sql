f4c2aea40279d8f9b589705df2b0283f9acda123
Moseh-sudo-25 <mosesnganga186@gmail.com>
2024-05-23 02:39:29 -0700

    err02

--- 0x0D-SQL_introduction/2-remove_database.sql
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -1,1 +1,6 @@
+-- Create database of the hbtn_0d_2 and user user_0d_2
+
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;
 
