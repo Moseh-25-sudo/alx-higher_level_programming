cfa1cca6c23234c22d7836de7619950e3b330586
Moseh-sudo-25 <mosesnganga186@gmail.com>
2024-05-23 01:46:19 -0700

    err001

--- 0x0D-SQL_introduction/0-list_databases.sql
+++ 0x0D-SQL_introduction/0-list_databases.sql
@@ -1,5 +1,4 @@
 /* List databases */
 
-SHOW GRANTS FOR 'user_0d_1'@'localhost'; -- show grants
-SHOW GRANTS FOR 'user_0d_2'@'localhost'; -- show grants
-
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
